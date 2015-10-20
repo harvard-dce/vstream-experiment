f9452510a09d93cbe68159dc0cd270e7aa847441
Jim Kang <jimkang@gmail.com>
2015-10-20 12:11:15 -0400

    Refactored so that we can easily switch between running buffer-all and buffer-when-needed examples.

---
+++ buffer-all.js
@@ -0,0 +1,47 @@
+function bufferAll(opts) {
+  var video;
+  var url;
+  var mimeCodec;
+
+  if (opts) {
+    video = opts.video;
+    url = opts.url;
+    mimeCodec = opts.mimeCodec;
+  }
+
+  if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
+    var mediaSource = new MediaSource;
+    //console.log(mediaSource.readyState); // closed
+    video.src = URL.createObjectURL(mediaSource);
+    mediaSource.addEventListener('sourceopen', sourceOpen);
+  } else {
+    console.error('Unsupported MIME type or codec: ', mimeCodec);
+  }
+
+  function sourceOpen (_) {
+    //console.log(this.readyState); // open
+    var mediaSource = this;
+    var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+    fetchAB(url, function (buf) {
+      sourceBuffer.addEventListener('updateend', function (_) {
+        mediaSource.endOfStream();
+        video.play();
+        //console.log(mediaSource.readyState); // ended
+      });
+      sourceBuffer.appendBuffer(buf);
+    });
+  };
+
+  function fetchAB (url, cb) {
+    console.log(url);
+    var xhr = new XMLHttpRequest;
+    xhr.open('get', url);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function () {
+      cb(xhr.response);
+    };
+    xhr.send();
+  };
+}
+
+module.exports = bufferAll;
--- index.js
+++ index.js
@@ -1,44 +1,23 @@
-// Adapter from https://github.com/nickdesaulniers/netfix/blob/gh-pages/demo/bufferAll.html.
+var bufferWhenNeeded = require('./buffer-when-needed');
+var bufferAll = require('./buffer-all');
+var clone = require('lodash.clone');
 
-var video = document.querySelector('#presenter-video');
-
-var assetURL = 'data/presenter_trimmed.mp4';
 // Need to be specific for Blink regarding codecs
 // ./mp4info frag_bunny.mp4 | grep Codec
-var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
-debugger;
-if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
-  var mediaSource = new MediaSource;
-  video.src = URL.createObjectURL(mediaSource);
-  mediaSource.addEventListener('sourceopen', sourceOpen);
-}
-else {
-  console.error('Unsupported MIME type or codec: ', mimeCodec);
-}
+// var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
 
-function sourceOpen() {
-  //console.log(this.readyState); // open
-  var mediaSource = this;
-  var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-  fetchAB(assetURL, function (buf) {
-    sourceBuffer.addEventListener('updateend', respondToStreamEnd);
-    sourceBuffer.appendBuffer(buf);
-  });
-}
-
-function respondToStreamEnd() {
-  mediaSource.endOfStream();
-  video.play();
-  //console.log(mediaSource.readyState); // ended
-}
-
-function fetchAB(url, cb) {
-  console.log(url);
-  var xhr = new XMLHttpRequest;
-  xhr.open('get', url);
-  xhr.responseType = 'arraybuffer';
-  xhr.onload = function () {
-    cb(xhr.response);
-  };
-  xhr.send();
+var media = {
+  definitelyWorks: {
+    url: 'data/frag_bunny.mp4',
+    mimeCodec: 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+  },
+  dceExample: {
+    url: 'data/presenter_trimmed.mp4',
+    mimeCodec: 'video/mp4; codecs="avc1.640028, mp4a.40.2"'
+  }
 };
+
+var opts = clone(media['definitelyWorks']);
+opts.video = document.querySelector('#presenter-video');
+bufferWhenNeeded(opts);
+bufferAll(opts);
