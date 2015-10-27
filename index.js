733bf25e6f7d8ffc663e1529c12c847cce48356b
Jim Kang <jimkang@gmail.com>
2015-10-27 16:01:25 -0400

    Added transcode/checking targets.

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ var media = {
   dceExample: {
     url: 'data/presenter_trimmed.mp4',
     mimeCodec: 'video/mp4; codecs="avc1.640028, mp4a.40.2"'
+  },
+  transcodeA: {
+    url: 'data/1min-encoded.mp4',
+    mimeCodec: 'video/mp4; codecs="avc1.42c01E,mp4a.40.2"'
   }
 };
 
-var opts = clone(media['definitelyWorks']);
+var opts = clone(media['transcodeA']);
 opts.video = document.querySelector('#presenter-video');
-bufferWhenNeeded(opts);
+// bufferWhenNeeded(opts);
 bufferAll(opts);
