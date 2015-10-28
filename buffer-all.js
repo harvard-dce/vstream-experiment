784e4deb0976f6627178cb5b9034bd873ea308fe
Jim Kang <jimkang@gmail.com>
2015-10-27 23:02:06 -0400

    Added status message.

--- buffer-all.js
+++ buffer-all.js
@@ -10,21 +10,24 @@ function bufferAll(opts) {
   }
 
   if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
+    document.querySelector('#message').textContent = 'Codec is supported.';
     var mediaSource = new MediaSource;
     //console.log(mediaSource.readyState); // closed
     video.src = URL.createObjectURL(mediaSource);
     mediaSource.addEventListener('sourceopen', sourceOpen);
   } else {
+    document.querySelector('#message').textContent = 'Codec is NOT supported.';
     console.error('Unsupported MIME type or codec: ', mimeCodec);
   }
 
   function sourceOpen (_) {
-    //console.log(this.readyState); // open
+    console.log(this.readyState); // open
     var mediaSource = this;
     var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
     fetchAB(url, function (buf) {
       sourceBuffer.addEventListener('updateend', function (_) {
-        mediaSource.endOfStream();
+        console.log(mediaSource.readyState);
+        // mediaSource.endOfStream();
         video.play();
         //console.log(mediaSource.readyState); // ended
       });
--- index.js
+++ index.js
@@ -14,10 +14,14 @@ var media = {
   dceExample: {
     url: 'data/presenter_trimmed.mp4',
     mimeCodec: 'video/mp4; codecs="avc1.640028, mp4a.40.2"'
+  },
+  baseline: {
+    url: 'data/baseline3_test.mp4',
+    mimeCodec: 'video/mp4; codecs="avc1.42c01E, mp4a.40.2"'
   }
-};
+}
 
 var opts = clone(media['definitelyWorks']);
 opts.video = document.querySelector('#presenter-video');
-bufferWhenNeeded(opts);
+// bufferWhenNeeded(opts);
 bufferAll(opts);
